291ac6754966b5a1b6400a4c066db850bac69a7f
Jacky Zhao <j.zhao2k19@gmail.com>
2022-03-24 11:55:42 -0700

    base class refactor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { WebsocketProvider } from "y-websocket"
 import * as Y from "yjs"
 import { nanoid } from "nanoid"
 import randomcolor from "randomcolor"
+import { PerfectCursor } from "perfect-cursors"
 
 interface Cursor {
   id: string
@@ -11,58 +12,112 @@ interface Cursor {
   y: number
   chat: string
   lastSent: string
+  el: HTMLElement | undefined
+  pc: PerfectCursor | undefined
 }
 
-// TODO: abstract this into a class that gets initialized
+type ReplicatedCursor = Pick<Cursor, "id" | "color" | "x" | "y" | "chat" | "lastSent">;
 
-const self = nanoid()
-const room_id = `cursor-chat-room-${window.location.host + window.location.pathname}`
-const doc: Y.Doc = new Y.Doc()
-const provider = new WebsocketProvider(
-  "wss://demos.yjs.dev",
-  room_id,
-  doc
-)
+class CursorChat {
+  self_id: string
+  room_id: string
+  doc: Y.Doc
+  provider: WebsocketProvider
+  me: Cursor
+  others: Map<string, Cursor>
+  replicated_cursors: Y.Map<ReplicatedCursor>
+  cursorLayerDiv: HTMLElement
+  intervalId: number
 
-const me: Cursor = {
-  id: self,
-  color: randomcolor({
-    hue: "#e46262",
-  }),
-  x: 0,
-  y: 0,
-  chat: "",
-  lastSent: (new Date(0)).toUTCString()
+  constructor(divId = "cursor-chat-layer", wsProvider = "wss://demos.yjs.dev") {
+    const ld = document.getElementById(divId)
+    if (!ld) {
+      throw `Couldn't find div with id ${divId} Make sure DOM content is fully loaded before initializing`
+    }
+    this.cursorLayerDiv = ld
+
+    this.self_id = nanoid()
+    this.room_id = `cursor-chat-room-${window.location.host + window.location.pathname}`
+    this.doc = new Y.Doc()
+    this.provider = new WebsocketProvider(
+      wsProvider,
+      this.room_id,
+      this.doc
+    )
+
+    // initialize self
+    this.me = {
+      id: this.self_id,
+      color: randomcolor({
+        hue: "#e46262",
+      }),
+      x: 0,
+      y: 0,
+      chat: "",
+      lastSent: (new Date(0)).toUTCString(),
+      el: undefined,
+      pc: undefined,
+    }
+
+    this.replicated_cursors = this.doc.getMap('state')
+    this.replicated_cursors.clear()
+    this.others = new Map()
+
+    // attach mouse listener to update self object
+    document.onmousemove = (evt) => {
+      this.me.x = evt.pageX;
+      this.me.y = evt.pageY;
+    }
+
+    // setup replication
+    this.intervalId = setInterval(() => {
+      console.log(this.replicated_cursors)
+      // push self
+      this.replicated_cursors.set(this.self_id, this.me)
+
+      // update local cache
+      this.replicated_cursors.forEach((cursor: ReplicatedCursor) => {
+        if (cursor.id !== this.self_id) {
+          if (this.others.has(cursor.id)) {
+            // in cache, update
+            const concrete = this.others.get(cursor.id) as Cursor
+            concrete.pc?.addPoint([cursor.x, cursor.y])
+            const updatedConcrete = {
+              ...concrete,
+              ...cursor,
+            }
+            this.others.set(cursor.id, updatedConcrete)
+          } else {
+            // new cursor, register and add to dom
+            const concrete = initializeCursor(cursor)
+            if (concrete.el) {
+              concrete.el.classList.add("new")
+              this.cursorLayerDiv.appendChild(concrete.el)
+            }
+            this.others.set(cursor.id, concrete)
+          }
+        }
+      })
+
+      // delete old records
+      this.others.forEach((cursor: Cursor) => {
+        if (!this.replicated_cursors.has(cursor.id)) {
+          deleteCursor(cursor)
+        }
+      })
+    }, 80)
+  }
+
+  destroy() {
+    // TODO: properly clean up resources
+    this.replicated_cursors.delete(this.self_id)
+    clearInterval(this.intervalId)
+  }
 }
 
-// register handler to update the self object
-document.addEventListener("DOMContentLoaded", () => {
-  document.onmousemove = (evt) => {
-    me.x = evt.pageX;
-    me.y = evt.pageY;
-  }
-})
-
-const state: Y.Map<Cursor> = doc.getMap('state')
-state.clear()
-
-// target is 80ms
-setInterval(() => {
-  state.set(self, me)
-}, 80)
-
-// TODO: observe state and update clientSideState as appropriate
-// force update of all clients we track
-// remove old, add new, update existing
-const clientSideState: Record<string, Cursor> = {}
-
-function initializeCursor(c: Cursor) {
-  if (c.id === self) {
-    throw "Shouldn't render your own cursor!"
-  }
-  const cursorLayerDiv = document.getElementById("cursor-chat-layer")
+function initializeCursor(c: ReplicatedCursor): Cursor {
   const htmlFragment = `<svg
-    id="${c.id}"
+    id="cursor_${c.id}"
     class="cursor"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 35 35"
@@ -85,10 +140,28 @@ function initializeCursor(c: Cursor) {
 
   const template = document.createElement('template')
   template.innerHTML = htmlFragment
-  const cursor = template.content.firstChild as ChildNode
-  cursorLayerDiv?.appendChild(cursor)
+  const cursorEl = template.content.firstChild as HTMLElement
+  const concreteCursor: Cursor = {
+    ...c,
+    el: cursorEl,
+    pc: new PerfectCursor((point: number[]) => {
+      [c.x, c.y] = point
+      cursorEl.style.setProperty("transform", `translate(${c.x}px, ${c.y}px)`)
+    })
+  }
+  return concreteCursor
 }
 
-function updateCursor(c: Cursor) {
-  // TODO: follow https://github.com/steveruizok/perfect-cursors
+function deleteCursor(c: Cursor) {
+  const cursorEl = document.getElementById(`cursor_${c.id}`)
+  if (c.pc) {
+    c.pc.dispose()
+  }
+  if (cursorEl) {
+    cursorEl.remove()
+  } else {
+    throw `Warning: couldn't find cursor with id ${c.id}!`
+  }
 }
+
+const _ = new CursorChat()
