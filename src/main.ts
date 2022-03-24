165e529d3a14954c5d696e647a3a1cf6a5628fbf
Jacky Zhao <j.zhao2k19@gmail.com>
2022-03-24 15:40:41 -0700

    base chat styling

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,10 @@ interface Cursor {
   y: number
   chat: string
   nStale: number
-  lastSent: string
   pc: PerfectCursor | undefined
 }
 
-type ReplicatedCursor = Pick<Cursor, "id" | "color" | "x" | "y" | "chat" | "lastSent">;
+type ReplicatedCursor = Pick<Cursor, "id" | "color" | "x" | "y" | "chat">;
 
 class CursorChat {
   self_id: string
@@ -53,13 +52,12 @@ class CursorChat {
     this.me = {
       id: this.self_id,
       color: randomcolor({
-        hue: "#e46262",
+        luminosity: 'light',
       }),
       x: 0,
       y: 0,
       chat: "",
       nStale: 0,
-      lastSent: (new Date(0)).toUTCString(),
       pc: undefined,
     }
 
@@ -135,27 +133,28 @@ class CursorChat {
 }
 
 function initializeCursor(c: ReplicatedCursor, div: HTMLElement): Cursor {
-  const htmlFragment = `<svg
-    id="cursor_${c.id}"
-    class="cursor"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 35 35"
-    fill="none"
-    fillRule="evenodd"
-  >
-    <g fill="rgba(0,0,0,.2)" transform="translate(1,1)">
-      <path d="m12 24.4219v-16.015l11.591 11.619h-6.781l-.411.124z" />
-      <path d="m21.0845 25.0962-3.605 1.535-4.682-11.089 3.686-1.553z" />
-    </g>
-    <g fill="white">
-      <path d="m12 24.4219v-16.015l11.591 11.619h-6.781l-.411.124z" />
-      <path d="m21.0845 25.0962-3.605 1.535-4.682-11.089 3.686-1.553z" />
-    </g>
-    <g fill="${c.color}">
-      <path d="m19.751 24.4155-1.844.774-3.1-7.374 1.841-.775z" />
-      <path d="m13 10.814v11.188l2.969-2.866.428-.139h4.768z" />
-    </g>
-  </svg>`
+  const htmlFragment = `<div id="cursor_${c.id}" class="cursor">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 35 35"
+      fill="none"
+      fillRule="evenodd"
+    >
+      <g fill="rgba(0,0,0,.2)" transform="translate(1,1)">
+        <path d="m12 24.4219v-16.015l11.591 11.619h-6.781l-.411.124z" />
+        <path d="m21.0845 25.0962-3.605 1.535-4.682-11.089 3.686-1.553z" />
+      </g>
+      <g fill="white">
+        <path d="m12 24.4219v-16.015l11.591 11.619h-6.781l-.411.124z" />
+        <path d="m21.0845 25.0962-3.605 1.535-4.682-11.089 3.686-1.553z" />
+      </g>
+      <g fill="${c.color}">
+        <path d="m19.751 24.4155-1.844.774-3.1-7.374 1.841-.775z" />
+        <path d="m13 10.814v11.188l2.969-2.866.428-.139h4.768z" />
+      </g>
+    </svg>
+    <p id="chat_${c.id}" style="background-color: ${c.color}">yaba daba doooooooo but make that shit so fucking long it doesn't fit on one line any more</p>
+  </div>`
 
   const template = document.createElement('template')
   template.innerHTML = htmlFragment
